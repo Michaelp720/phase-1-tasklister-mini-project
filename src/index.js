6fc87fba7d3c4dfc8200f28b25dbe70244ade01b
MichaelPile <mjpile@gmail.com>
2023-12-17 13:38:55 -0800

    completes task lister challenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  document.querySelector(`#create-task-form`).addEventListener("submit", addToTodo);
+  
+  function addToTodo(e){
+    e.preventDefault(); //prevents default behavior of submit event e
+    
+    const newTaskDescription = document.querySelector('#new-task-description').value;
+    //initializes a variable to the user typed value
+    const newTask = document.createElement('li'); //creates a new list item
+    newTask.appendChild(document.createTextNode(newTaskDescription)); //adds newTaskDescription as text of new list item
+    document.querySelector('#tasks').appendChild(newTask); //adds new list item to tasks list
+    document.querySelector('#new-task-description').value = ''; //resets user entered text
+  }
 });
